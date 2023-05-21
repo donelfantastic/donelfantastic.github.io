39f617c199bf333cf7b2add70b7fe94ea2e5c097
Donel Fantastic <121449182+donelfantastic@users.noreply.github.com>
2023-05-21 09:33:42 +0700

    Update min.script.js

--- src/min.script.js
+++ src/min.script.js
@@ -183,3 +183,14 @@ var $ul = $("#ytpl-thumbs");
     );
   };
 })();
+
+var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,4767284,4,2050,280,25,00010000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();
