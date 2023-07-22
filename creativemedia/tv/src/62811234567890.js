c6d49070fa9a304131b698a700cca1c26dcb6071
Donel Fantastic <121449182+donelfantastic@users.noreply.github.com>
2023-07-22 11:50:53 +0700

    Create 62811234567890.js

---
+++ creativemedia/tv/src/62811234567890.js
@@ -0,0 +1,19 @@
+var MLKJIHGFEDCBA2 = atob("aHR0cHM6Ly9wcm94eTIudm54c2VydmVycy5jb20v", "aHR0cHM6XC9cL3Byb3h5Mi52bnhzZXJ2ZXJzLmNvbVwv");
+var currentLocation = window.location.search;
+var stream_url = currentLocation.replace("?play&mediaType=all&live=true&url=", ""); 
+//var MLKJIHGFEDCBA = atob("aHR0cHM6Ly9wcm94eS52bnhzZXJ2ZXJzLmNvbS8=", "aHR0cHM6XC9cL3Byb3h5LnZueHNlcnZlcnMuY29tXC8=");
+var ZXCVBNM = atob("aHR0cHM6Ly9jb3JzcHJveHkucGxlbnR5Z3JhbS5jb20v", "aHR0cHM6XC9cL2NvcnNwcm94eS5wbGVudHlncmFtLmNvbVwv");
+//var ABCDEFGHIJKLM2 = MLKJIHGFEDCBA + stream_url;
+var NOPQRSTUVWXYZ ="";
+var ANBOCPDQERFSG ="";
+var QWERTYUIOP = {
+  "com.widevine.alpha": ZXCVBNM +"https:\/\/mrpw.ptmnc01.verspective.net\/?deviceId=YzM0NDA0MTItMGYyMC0zNGQ0LTliMjMtNDY4MjE1ZjA5NmZj",        
+          "com.microsoft.playready": ""
+   };
+var QWERTY123 = {
+  "com.widevine.alpha": ZXCVBNM +"https:\/\/mrpw.ptmnc01.verspective.net\/?deviceId=YzM0NDA0MTItMGYyMC0zNGQ0LTliMjMtNDY4MjE1ZjA5NmZj"        
+   };
+var ASDFGHJKL = {
+  "82f8e2a17dac44c0a18f660479349c59":"1b144f73e6fefe91cd05f850e2b589d0", 
+  "cfb5e2b73bef4f3c878f25ab86a7451f":"0d6712bf2a84edcc93d001a9613f6fec"
+};
