283d1542f3f4d693e7ce9a06b4bbec03c2c7bcfb
Donel Fantastic <121449182+donelfantastic@users.noreply.github.com>
2024-08-25 21:27:14 +0700

    Update script.js

--- src/latest/script.js
+++ src/latest/script.js
@@ -281,7 +281,7 @@ window.addEventListener("load", function () {
         ) {
           b++;
 
-          var DoryHarsa = "https://donelfantastic.github.io:443/mn/embed/cloppr/?live&url=";
+          var DoryHarsa = "https://donelfantastic.github.io/mn/embed/cloppr/?live&url=";
           //var NellaKharisma = "https://donelfantastic.github.io:443/mn/embed/cloppr/?live&url=";
           var deviceId = "deviceId=YzM0NDA0MTItMGYyMC0zNGQ0LTliMjMtNDY4MjE1ZjA5NmZj";   
           //output += "<tr><td><center><img width='64' src='" + a[i].logo + "'/></center></td><td>" + a[i].name + "</td><td style='display:none;'>" + d + "</td><td><center><a class='open-video' style='text-decoration:none;' href='" + Dory + a[i].url + "?" + deviceId + "'><img src='//donelfantastic.github.io/assets/watchnow3.png' style='width:48px;height:auto;'/></a></center></td></tr>";
